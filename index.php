5d65d7df4212615d4a91fc1e830e733cd2c506be
TTY Demicchi <m@v.nu>
2017-10-11 23:33:38 +0900

    ファイル名指定できるようにした

--- index.php
+++ index.php
@@ -13,8 +13,13 @@ if (@$_POST["getpdf"] === "true") {
     $totalwidth = 0;
     $maxheight = intval($_POST["maxheight"]);
     $combinepdf = (@$_POST["combinepdf"] === "true");
+    $volnum = $_POST["volnum"];
+    $pdfname = ($_POST["pdfname"] == "") ? "merged.pdf" : str_replace('%n', $volnum, $_POST["pdfname"].".pdf");
+    $pngname = ($_POST["pngname"] == "") ? "merged.png" : str_replace('%n', $volnum, $_POST["pngname"].".png");
+    $zipname = ($_POST["zipname"] == "") ? "pack.zip" : str_replace('%n', $volnum, $_POST["zipname"].".zip");
+    $forwin = (@$_POST["forwin"] === "true");
 
-    if ($maxheight < 1 || $maxheight > 5000) {
+    if ($maxheight < 100 || $maxheight > 5000) {
         $maxheight = 0;
     } else {
         $maxheight = $_POST["maxheight"];
@@ -100,44 +105,85 @@ if (@$_POST["getpdf"] === "true") {
     $zipfile = tempnam(sys_get_temp_dir(), "pdf2png");
     $zip = new ZipArchive;
     if ($zip->open($zipfile, ZipArchive::CREATE) === true) {
-        $zip->addFromString('merged.png', $resultimage_string);
+        $zip->addFromString($forwin ? mb_convert_encoding($pngname, "CP932", "UTF-8") : $pngname, $resultimage_string);
         if ($combinepdf)
-            $zip->addFromString('merged.pdf', $resultpdf_string);
+            $zip->addFromString($forwin ? mb_convert_encoding($pdfname, "CP932", "UTF-8") : $pdfname, $resultpdf_string);
         $zip->close();
     }
 
     header('Content-Type: application/zip');
-    header('Content-disposition: attachment; filename="pack.zip"');
+    header('Content-disposition: attachment; filename="'.$zipname.'"');
     header('Content-Length: ' . filesize($zipfile));
     readfile($zipfile);
     unlink($zipfile);
 
 } else {
+    $volnum = (isset($_GET["volnum"])) ? htmlspecialchars($_GET["volnum"]) : "";
+    $pdfname = (isset($_GET["pdfname"])) ? htmlspecialchars($_GET["pdfname"]) : "";
+    $pngname = (isset($_GET["pngname"])) ? htmlspecialchars($_GET["pngname"]) : "";
+    $zipname = (isset($_GET["zipname"])) ? htmlspecialchars($_GET["zipname"]) : "";
+    $forwin = (isset($_GET["forwin"])) ? (($_GET["forwin"] === "true") ? "checked=\"checked\"" : "") : "checked=\"checked\"";
+
     echo <<<HTML
+<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8" />
         <title>PDF2PNG</title>
+        <script>
+        function getlink() {
+        	document.getElementById('link').innerHTML = 
+            	"Bookmark Me! <a href=\"/?volnum=" + encodeURIComponent(document.forms.uploadform.volnum.value) + 
+            	"&pdfname=" + encodeURIComponent(document.forms.uploadform.pdfname.value) + 
+            	"&pngname=" + encodeURIComponent(document.forms.uploadform.pngname.value) + 
+            	"&zipname=" + encodeURIComponent(document.forms.uploadform.zipname.value) + 
+            	"&forwin=" + (document.forms.uploadform.forwin.checked ? "true" : "false") + 
+            	"\">PDF2PNG</a>";
+        }
+        </script>
     </head>
     <body>
         <h1>PDF2PNG</h1>
-        All PDFs should be with version 1.4 (Acrobat 5.x) or below to combine.<br /><br />
-        <form action="/" method="post" enctype="multipart/form-data">
+        Merge pdfs and export in one PNG and one PDF.<br />
+        <br />
+        <form name="uploadform" action="/" method="post" enctype="multipart/form-data">
+            <h2>Files to Process</h2>
             PDF1: <input name="pdf[]" type="file" /><br />
             PDF2: <input name="pdf[]" type="file" /><br />
             PDF3: <input name="pdf[]" type="file" /><br />
             PDF4: <input name="pdf[]" type="file" /><br />
             PDF5: <input name="pdf[]" type="file" /><br />
+            - Up to 1 GB total.
             <br />
+            <br />
+            <h2>Process Config</h2>
             <input id="combinepdf_chk" name="combinepdf" type="checkbox" value="true" checked="checked" /><label for="combinepdf_chk">Merge all PDFs into one PDF</label><br />
+            - All PDFs should be with version 1.4 (Acrobat 5.x) or below to merge into one PDF.<br />
+            - PowerPoint exports version 1.5 PDFs but they seem to be compatible to this PDF2PNG.<br />
             <br />
             <input id="reverse_chk" name="reverse" type="checkbox" value="true" checked="checked" /><label for="reverse_chk">Merge in reverse order for PNG (Japanese Newspaper)</label><br />
-            Max PNG Height: <input name="maxheight" type="text" size="6" pattern="^[0-9]+$" value="1000" /> (range: 1-5000, 0 means default)<br />
+            Max PNG Height: <input name="maxheight" type="text" size="6" pattern="^[0-9]+$" value="1000" /> (range: 100-5000, 0 means default)<br />
             <input name="getpdf" type="hidden" value="true" />
             <br />
-            <input type="submit" value="Generate PNG" />
+            <br />
+            <h2>Filename Config</h2>
+            Volume Number (if necessary in filenames): <input name="volnum" size="10" type="text" value="{$volnum}" /><br />
+            - %n in the fields below will be replaced by the value Volume Number above. <br />
+            PDF Name: <input name="pdfname" size="20" type="text" value="{$pdfname}" />.pdf (default: merged.pdf)<br />
+            PNG Name: <input name="pngname" size="20" type="text" value="{$pngname}" />.png (default: merged.png)<br />
+            ZIP Name: <input name="zipname" size="20" type="text" value="{$zipname}" />.zip (default: pack.zip)<br />
+            Filename Charset for Windows Japanese(CP932): <input id="forwin_chk" name="forwin" type="checkbox" value="true" {$forwin} /><label for="forwin_chk">Yes</label><br />
+            <input type="button" value="Get Link to Bookmark with the Filename Parameters Above" onclick="getlink()" /><br />
+            <div id="link"></div>
+            <br />
+            <br />
+            <h2>Get Ready?</h2>
+            <input type="submit" value="Upload PDFs and Generate PNG and PDF" />
+            <br />
+            <br />
         </form>
         <hr />
-        Ver.20171009-01
+        Ver.20171011-01
     </body>
 </html>
 HTML;
